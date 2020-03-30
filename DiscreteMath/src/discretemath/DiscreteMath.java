de21aec846b4542db986b3effcc7848aeb09bb90
Lucas Santos <42410626+luksantos@users.noreply.github.com>
2020-03-30 16:04:31 -0300

    Contido/Igual - União

--- DiscreteMath/src/discretemath/DiscreteMath.java
+++ DiscreteMath/src/discretemath/DiscreteMath.java
@@ -6,6 +6,7 @@
 package discretemath;
 
 import java.io.*;
+import java.util.*;
 
 /**
  *
@@ -14,16 +15,52 @@ import java.io.*;
 public class DiscreteMath {
 
     public static void main(String[] args) throws Exception {
-        
-        BufferedReader br = new BufferedReader(new FileReader("sets.txt"));
 
+        BufferedReader br = new BufferedReader(new FileReader("sets.txt"));
         String txt;
         while ((txt = br.readLine()) != null) {
             System.out.println("\n" + txt);
-            String conjunto[] = txt.split(", ");
+            String[] conjunto = txt.split("[ABCD=,{ }]");
             for (String item : conjunto) {
                 System.out.println(item);
             }
         }
+        
+        Conjunto conj = new Conjunto();
+        Scanner ler = new Scanner(System.in);
+        int opcao = 0;
+        do {
+            System.out.println("\n\n### Menu ###");
+            System.out.println("\n===============================");
+            System.out.println("|     0 - Sair");
+            System.out.println("|     1 - Percente/Não Pertence");
+            System.out.println("|     2 - Contido/Igual");
+            System.out.println("|     3 - União");
+            System.out.println("=================================\n");
+            
+            System.out.print("Selecione a Opção:");
+            opcao = ler.nextInt();
+            
+            switch (opcao) {
+                case 1:
+                    System.out.println("Pertence: " + conj.Pertence());
+                    break;
+                case 2:
+                    System.out.println("Contido/Igual: " + conj.ContidoIgual());
+                    break;
+                case 3: 
+                    System.out.println("União: AUX=" + conj.União());
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opção Inválida!");
+                    break;
+            }
+        } while (opcao != 0);
+
+        
+        
+
     }
 }
