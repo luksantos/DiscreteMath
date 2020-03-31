e6cd5bae5ca0450661b4ed655b9457c1ac8f6a11
Lucas Santos <42410626+luksantos@users.noreply.github.com>
2020-03-31 15:50:36 -0300

    Correções - Contido Propriamente - Interceção

--- DiscreteMath/src/discretemath/Conjunto.java
+++ DiscreteMath/src/discretemath/Conjunto.java
@@ -13,31 +13,128 @@ import java.util.*;
  */
 public class Conjunto {
 
-    ArrayList<Integer> conj1 = new ArrayList();
+    int conj1[] = {3, 4, -1, 5};
+    int conj2[] = {4, 6, 7, 9};
+    //int conj2[] = {4, 3, 5, -1};
     Scanner ler = new Scanner(System.in);
 
     public Conjunto() {
-        conj1.add(3);
-        conj1.add(4);
-        conj1.add(-1);
-        conj1.add(5);
     }
 
-    public boolean pertence() {
+    public boolean Pertence() {
+        Scanner ler = new Scanner(System.in);
+        int aux = 0;
         boolean option = false;
-        for (Integer c : conj1) {
-            int n;
-            System.out.printf("Insira um Número:\n");
-            n = ler.nextInt();
-            if (conj1.contains(n)) {
-                option = true;
-                break;
-            } else {
-                option = false;
-                break;
+        do {
+            System.out.println("\n==Verificar ==");
+            System.out.println("|  1 - Conj. 1");
+            System.out.println("|  2 - Conj. 2");
+            System.out.print("Selecione a Opção:");
+            aux = ler.nextInt();
+
+            switch (aux) {
+                case 1:
+                    int i,
+                     n;
+                    System.out.printf("Insira um Número:\n");
+                    n = ler.nextInt();
+                    for (i = 0; i < conj1.length;) {
+                        if (n == conj1[i]) {
+                            option = true;
+                            break;
+                        } else {
+                            option = false;
+                            i++;
+                        }
+                    }
+                    return option;
+                case 2:
+                    int j;
+                    System.out.printf("Insira um Número:\n");
+                    n = ler.nextInt();
+                    for (j = 0; j < conj1.length;) {
+                        if (n == conj2[j]) {
+                            option = true;
+                            break;
+                        } else {
+                            option = false;
+                            j++;
+                        }
+                    }
+                    return option;
+            }
+        } while (aux != 0);
+        return option;
+    }
+
+    public boolean ContidoIgual() {
+        boolean c = false;
+        ArrayList<Integer> aux = new ArrayList();
+        int i, j;
+        for (i = 0; i < conj1.length; i++) {
+            for (j = 0; j < conj2.length;) {
+                if (conj1[i] != conj2[j]) {
+                    j++;
+                } else {
+                    aux.add(conj1[i]);
+                    break;
+                }
             }
         }
-        return option;
+        if (conj2.length == aux.size()) {
+            c = true;
+        }
+        return c;
+    }
+    
+    public boolean ContidoPropriamente() {
+        boolean c = false;
+        ArrayList<Integer> aux = new ArrayList();
+        int i, j;
+        for (i = 0; i < conj1.length; i++) {
+            for (j = 0; j < conj2.length;) {
+                if (conj1[i] != conj2[j]) {
+                    j++;
+                } else {
+                    aux.add(conj1[i]);
+                    break;
+                }
+            }
+        }
+        if (conj2.length > aux.size()) {
+            c = true;
+        }
+        return c;
+    }
+
+    public String União() {
+        String valores;
+        Set<Integer> conjAux = new HashSet<>();
+        int i, j;
+        for (i = 0; i < conj1.length; i++) {
+            for (j = 0; j < conj2.length; j++) {
+                conjAux.add(conj2[j]);
+            }
+            conjAux.add(conj1[i]);
+        }
+        valores = conjAux.toString();
+        return valores;
+    }
+
+    public String Intercecao() {
+        ArrayList<Integer> aux = new ArrayList();
+        int i, j;
+        for (i = 0; i < conj1.length; i++) {
+            for (j = 0; j < conj2.length;) {
+                if (conj1[i] != conj2[j]) {
+                    j++;
+                } else {
+                    aux.add(conj1[i]);
+                    break;
+                }
+            }
+        }
+        return aux.toString();
     }
 
 }
